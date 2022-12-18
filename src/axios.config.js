2aea4d5d7ff397cb0918020952b7238a376ac332
Pigeon377 <892197240@qq.com>
2022-12-18 17:54:21 +0800

    fix(axios.config): add no cache header for cdn

--- src/axios.config.js
+++ src/axios.config.js
@@ -1,23 +1,25 @@
 import axios from 'axios'
 // axios.defaults.baseURL = 'http://192.168.31.20:8080'
-axios.defaults.baseURL='http://www.avaqua.love:8080'
+// axios.defaults.baseURL = 'http://www.avaqua.love:8080'
+axios.defaults.baseURL = ''
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 
-const axiosInstance = axios.create({  
-	timeout: 20000,
-    headers: {
-        'Content-Type': 'application/json',
-    },
+const axiosInstance = axios.create({
+  timeout: 20000,
+  headers: {
+    'Content-Type': 'application/json',
+    'Cache-Control': 'no-cache'
+  },
 })
 axiosInstance.interceptors.request.use(function (config) {
-    let token = window.localStorage.getItem('token')
-    if (token) {
-      config.headers['Authorization'] = token
-    }
-    return config
-  }, function (error) {
-    return Promise.reject(error)
-  })
+  let token = window.localStorage.getItem('token')
+  if (token) {
+    config.headers['Authorization'] = token
+  }
+  return config
+}, function (error) {
+  return Promise.reject(error)
+})
 
 axiosInstance.interceptors.response.use(function (response) {
   return response;
